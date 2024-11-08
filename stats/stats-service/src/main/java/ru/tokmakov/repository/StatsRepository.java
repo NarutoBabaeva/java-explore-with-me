ac364406c6514746d8d647083a0441efa245bd7b
Yaroslav Tokmakov <ytokmakoff@gmail.com>
2024-11-08 21:59:51 +0300

    fix

--- stats/stats-service/src/main/java/ru/tokmakov/repository/StatsRepository.java
+++ stats/stats-service/src/main/java/ru/tokmakov/repository/StatsRepository.java
@@ -16,7 +16,7 @@ public interface StatsRepository extends JpaRepository<Hit, Long> {
            "FROM Hit h " +
            "WHERE h.timestamp BETWEEN :start AND :end " +
            "AND (:uris IS NULL OR h.uri IN :uris) " +
-           "GROUP BY h.app, h.uri " +
+           "GROUP BY h.uri " +
            "ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<StatsResponseDto> findAllHits(@Param("start") LocalDateTime start,
                                        @Param("end") LocalDateTime end,
@@ -26,7 +26,7 @@ public interface StatsRepository extends JpaRepository<Hit, Long> {
            "FROM Hit h " +
            "WHERE h.timestamp BETWEEN :start AND :end " +
            "AND (:uris IS NULL OR h.uri IN :uris) " +
-           "GROUP BY h.app, h.uri " +
+           "GROUP BY h.uri " +
            "ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<StatsResponseDto> findUniqueHits(@Param("start") LocalDateTime start,
                                           @Param("end") LocalDateTime end,
