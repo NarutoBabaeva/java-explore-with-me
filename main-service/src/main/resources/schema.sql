10fbdc80fd2662fbd55b9f7d7ba569384d77f446
Yaroslav Tokmakov <ytokmakoff@gmail.com>
2024-11-17 15:26:57 +0300

    fix: annotation not is text type

--- main-service/src/main/resources/schema.sql
+++ main-service/src/main/resources/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS compilations (
 
 CREATE TABLE IF NOT EXISTS events (
                         id BIGSERIAL PRIMARY KEY,
-                        annotation VARCHAR NOT NULL,
+                        annotation TEXT NOT NULL,
                         category_id BIGINT NOT NULL REFERENCES categories(id),
                         confirmed_requests INTEGER,
                         created_on TIMESTAMP,
