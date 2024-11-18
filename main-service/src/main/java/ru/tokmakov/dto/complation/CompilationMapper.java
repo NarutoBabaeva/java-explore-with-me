7b60c8d9880f0efbd07c1779da118b4aff552d85
Yaroslav Tokmakov <ytokmakoff@gmail.com>
2024-11-18 11:04:28 +0300

    fix

--- main-service/src/main/java/ru/tokmakov/dto/complation/CompilationMapper.java
+++ main-service/src/main/java/ru/tokmakov/dto/complation/CompilationMapper.java
@@ -9,7 +9,9 @@ public class CompilationMapper {
     public CompilationDto toCompilationDto(Compilation compilation) {
         CompilationDto compilationDto = new CompilationDto();
         compilationDto.setId(compilation.getId());
-        compilationDto.setEvents(compilation.getEvents().stream().map(EventMapper::toEventShortDto).toList());
+        if (compilation.getEvents() != null && !compilation.getEvents().isEmpty()) {
+            compilationDto.setEvents(compilation.getEvents().stream().map(EventMapper::toEventShortDto).toList());
+        }
         compilationDto.setPinned(compilation.getPinned());
         compilationDto.setTitle(compilation.getTitle());
         return compilationDto;
